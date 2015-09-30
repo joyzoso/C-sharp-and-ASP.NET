580c1557e45fcda70e24f274be3f6042b65a5247
joyzoso <joyrockon@gmail.com>
2015-09-29 23:15:57 -0700

    adding with arrays and double
    more examples

---
+++ exerciseB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//C# programming tutorial - Step by Step
+//Patrick WashingtonDC
+//examples for personal studying
+
+namespace exerciseB
+{
+    class exerciseB
+    {
+        static void Main()
+        {
+            string[] strs = { "3.45", "3.87", "87.98", "56.70" };
+
+            double total = 0.0;
+
+            foreach (string temp in strs)
+            {
+                total += Convert.ToDouble(temp);
+            }
+            Console.WriteLine("Total = " + total);
+        }
+    }
+
+}
