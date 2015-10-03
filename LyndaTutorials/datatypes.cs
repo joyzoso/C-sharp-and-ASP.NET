a3326a774be162f1848435f25e913fda2fa799a7
joyzoso <joyrockon@gmail.com>
2015-10-03 15:34:04 -0700

    LyndaTutorials and Exercises
    Up&Running with C#

---
+++ LyndaTutorials/datatypes.cs
@@ -0,0 +1,9 @@
+﻿1>------ Build started: Project: LanguageElements_CS, Configuration: Debug Any CPU ------
+1>C:\Users\student\Desktop\Ex_Files_UaR_CSharp\Exercise Files\LanguageElements_CS\LanguageElements_CS\Program.cs(19,19,19,30): warning CS0219: The variable 'aSignedByte' is assigned but its value is never used
+1>C:\Users\student\Desktop\Ex_Files_UaR_CSharp\Exercise Files\LanguageElements_CS\LanguageElements_CS\Program.cs(21,20,21,35): warning CS0219: The variable 'anUnsignedShort' is assigned but its value is never used
+1>C:\Users\student\Desktop\Ex_Files_UaR_CSharp\Exercise Files\LanguageElements_CS\LanguageElements_CS\Program.cs(22,19,22,25): warning CS0219: The variable 'aShort' is assigned but its value is never used
+1>C:\Users\student\Desktop\Ex_Files_UaR_CSharp\Exercise Files\LanguageElements_CS\LanguageElements_CS\Program.cs(24,18,24,31): warning CS0219: The variable 'anUnsignedInt' is assigned but its value is never used
+1>C:\Users\student\Desktop\Ex_Files_UaR_CSharp\Exercise Files\LanguageElements_CS\LanguageElements_CS\Program.cs(25,19,25,33): warning CS0219: The variable 'anUnsignedLong' is assigned but its value is never used
+1>C:\Users\student\Desktop\Ex_Files_UaR_CSharp\Exercise Files\LanguageElements_CS\LanguageElements_CS\Program.cs(28,20,28,27): warning CS0219: The variable 'aDouble' is assigned but its value is never used
+1>  LanguageElements_CS -> C:\Users\student\Desktop\Ex_Files_UaR_CSharp\Exercise Files\LanguageElements_CS\LanguageElements_CS\bin\Debug\LanguageElements_CS.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
