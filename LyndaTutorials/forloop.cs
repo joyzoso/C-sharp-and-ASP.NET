750a2f0d01e403ed9b0c8e5d12e7a277b4d4c7f7
joyzoso <joyrockon@gmail.com>
2015-10-03 17:58:54 -0700

    For Loop
    for loop drill

---
+++ LyndaTutorials/forloop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoopDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            for (int counter= 1; counter < 11; counter++)
+                Console.WriteLine(counter);
+        }
+
+
+
+    }
+}
